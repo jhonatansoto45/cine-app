13357feb6d9d0e0f3fb02ab3678bb3f0005e7de5
Jhonatan Soto <104380895+jhonatansoto45@users.noreply.github.com>
2023-01-25 13:51:57 -0500

    Delete file

--- src/environments/environment.backup.ts
+++
@@ -1,5 +0,0 @@
-export const credentials = {
-  baseUrl : 'https://imdb-top-100-movies.p.rapidapi.com/',
-  apiKey: 'a2b1438fd4msh4bccdc48b596242p122365jsn8b6a143c8cbe',
-  host: 'imdb-top-100-movies.p.rapidapi.com',
-}
